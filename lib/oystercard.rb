be4dabc2257b48027e8bfce950ca538f7e3120d9
PhilBligh <philbligh@hotmail.com>
2022-02-28 15:36:26 +0000

    finished touch in/out and journey status

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,32 @@
 class Oystercard
     MAX_LIMIT = 90
+
     def initialize
         @balance = 0
+        @status = false
     end
 
-    attr_accessor :balance
+    attr_accessor :balance, :status
 
     def top_up(amount)
-        fail "Cannot top up as you exceeded the maximum limit" if @balance + amount > MAX_LIMIT
+        fail "Cannot top up as you exceeded the maximum limit of #{MAX_LIMIT}" if @balance + amount > MAX_LIMIT
         @balance += amount
+    end
+
+    def deduct(fare)
+        @balance -= fare
+    end
+
+    def touch_in
+        @status = true
+    end
+
+    def touch_out
+        @status = false
+    end
+
+    def in_journey?
+        @status
     end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,8 +12,39 @@ describe Oystercard do
 
   it 'does not allow card to exceed max limit' do
     oyster_1 = Oystercard.new
+    max_balance = Oystercard::MAX_LIMIT
     expect do 
         oyster_1.top_up(91)
-    end.to raise_error "Cannot top up as you exceeded the maximum limit"
+    end.to raise_error "Cannot top up as you exceeded the maximum limit of #{max_balance}"
   end
+
+  it "deducts a fare" do
+    oyster_1 = Oystercard.new
+    oyster_1.top_up(15)
+    expect(oyster_1.deduct(10)).to eq (5)
+  end
+
+  describe ".touch_in" do
+    it 'changes the status of the oystercard to true' do
+      oyster_1 = Oystercard.new
+      expect(oyster_1.touch_in).to eq true
+    end
+  end
+
+  describe ".touch_out" do
+    it 'changes the status of the oystercard to false' do
+      oyster_1 = Oystercard.new
+      expect(oyster_1.touch_out).to eq false
+    end
+  end
+
+  describe ".in_journey?" do
+    it 'returns the journey status' do
+      oyster_1 = Oystercard.new
+      expect(oyster_1.in_journey?) == false
+      oyster_1.touch_in
+      expect(oyster_1.in_journey?) == true
+    end
+  end
+
 end
